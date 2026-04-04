50ad5b0a30e5ad7e507b39d23c02acefb24f5665
roger1717 <rogerhoyos80@gail.com>
2026-04-04 11:43:32 -0500

    curso

---
+++ src/06-comparacion.js
@@ -0,0 +1,19 @@
+
+// igualdad debil
+
+console.log(5 == '5');
+console.log(true == 1);
+console.log(false == 0);
+console.log(null = undefined);
+
+//desigualdad debil
+console.log(5 != '5');
+
+
+// igualdad estricta, verifica que los tipos sean iguales
+
+console.log( 5 === 5);
+console.log(5 === '5'); 
+
+// desigualdad estricta
+console.log(5 !== '5');
