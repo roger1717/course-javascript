9d7ab5326a5050dc16c7a39a552c4b9691aee604
roger1717 <rogerhoyos80@gail.com>
2026-04-21 16:45:11 -0500

    agregando cod

--- src/14-arrays.js
+++ src/14-arrays.js
@@ -6,13 +6,40 @@ const numeros = [1,2,3,4,5,6,7];
 const mixtos = [1,'texto',null,true,{id:1}];
 
 //CRUD  
+// push permite agregar un elemento al final. 
 notas.push('nota4');   // modifica el original
 console.log(notas);   
 
-// unshift
+// unshift   permite agregar al inicio del array
 notas.unshift('nota 0');
 console.log(notas)
 
 //splice
-notas.splice(1,0,'Notas 1.2');
-console.log(notas); 
+notas.splice(1,0,'Notas 1.2');   //1 es la posicion donde se va a agregar
+console.log(notas);              // 0 es para agregar , 1 para borrar lo que este en esa posicion.
+
+//leer (read) 
+console.log([notas[0]]);
+console.log([notas[1]]); 
+console.log(notas.length);    // Muestra la cantidad de elementos dentro del array.
+
+
+//Actualizar 
+
+const notas2 = ['nota1','nota2']
+notas2[1] = 'Notota1';
+console.log(notas2); 
+
+notas2.splice(2,0, 'Nota4');
+console.log(notas2);
+
+// Delete  (Eliminar)
+const notas3 = ['notas1','notas2'];
+console.log(notas3.pop()); 
+console.log(notas3); 
+
+console.log(notas3.push('notas3'))
+console.log(notas3);
+
+console.log(notas3.shift());
+console.log(notas3); 
--- src/practica_javas.js
+++ src/practica_javas.js
@@ -86,3 +86,93 @@ const resultado2 = procesarestudiantes(estudiantes);
 console.log("estudiantes que necesitan tutoria : ", resultado2.estudiante_nota_baja);
 console.log("promedio del curso : ", resultado2.promedioglobal);
 console.table(resultado2.listado_detallado); 
+
+
+
+// Gestion inventario de alimentos
+// arreglos con los nombres de los productos que pertenecen a la categoria. 
+
+
+const inventario = [
+    { nombre: "Leche", precio: 4.0, categoria: "Lácteos", estaVencido: false },
+    { nombre: "Yogur", precio: 2.0, categoria: "Lácteos", estaVencido: true },
+    { nombre: "Pan", precio: 1.5, categoria: "Panadería", estaVencido: false },
+    { nombre: "Queso", precio: 10.0, categoria: "Lácteos", estaVencido: true },
+    { nombre: "Manzana", precio: 0.8, categoria: "Frutas", estaVencido: false }
+];
+
+function procesarInventario(lista) {
+    let nombresLacteos = [];
+    let valorTotal = 0;
+
+    for (const item of lista) {
+        // 2. Filtrar por categoría "Lácteos" (Usa === para comparar texto)
+        if (item.categoria === "Lácteos"){
+            nombresLacteos.push(item.nombre);
+        }
+        // 3. Si estaVencido es true:
+        if (item.estaVencido){
+            item.precio = item.precio * 0.5;
+            item.nombre= "OFERTA - " + item.nombre;  
+        }
+        //    - Multiplica el precio por 0.5
+        valorTotal += item.precio;
+        
+    }
+
+    return {
+        soloLacteos: nombresLacteos,
+        inventarioTotal: valorTotal.toFixed(2),
+        listaFinal: lista
+    };
+}
+
+const resultado = procesarInventario(inventario);
+console.log("Productos Lácteos:", resultado.soloLacteos);
+console.log("Costo Total Inventario:", resultado.inventarioTotal);
+console.table(resultado.listaFinal);
+
+
+
+
+// carrito automatico 
+
+const carrito = [
+    {nombre: "Monitor 4k", precio:300, cantidad:2, enviogratis:true},
+    {nombre: "Mause gamer", precio:50, cantidad:2, enviogratis:false},
+    {nombre: "Teclado Mecanico", precio:150, cantidad:1, enviogratis:false},
+    {nombre: "Procesador i9", precio:300, cantidad:3, enviogratis:true},
+    {nombre: "Procesador i10", precio:350, cantidad:2, enviogratis:true},
+];
+
+function procesarcarrito(productos){
+    let pagaenvio = [];
+    let compratotal = 0; 
+
+    for (const itemcarro of productos){
+        
+        let subtotalItem = itemcarro.precio * itemcarro.cantidad;
+
+        if (compratotal > 500){
+            subtotalItem = subtotalItem * 0.90;
+            itemcarro.nombre= "OFERTA - " + itemcarro.nombre;
+            itemcarro.descuentoaplicado = true;
+        }
+        // revisar envio
+        if (itemcarro.enviogratis === false) {
+            pagaenvio.push(itemcarro.nombre);
+        }
+       
+        compratotal += subtotalItem;
+
+    }
+    return{
+        productosconenvio : pagaenvio,
+        total_a_pagar: compratotal.toFixed(2),
+        detallecarrito : productos
+    };
+}
+const resultado = procesarcarrito(carrito);
+console.log("productos que cobran envio: ", resultado.productosconenvio);
+console.log("total final de la orden: " , "$" + resultado.total_a_pagar);
+console.table(resultado.detallecarrito);  
